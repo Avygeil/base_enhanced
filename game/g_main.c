c9a16cb41cc1a5d3c4bc7b8425ebecf5725359d6
TheSil <sildak@gmail.com>
2015-08-15 08:27:41 +0200

    fixing siege timer during pause

--- game/g_main.c
+++ game/g_main.c
@@ -4296,6 +4296,14 @@ void G_RunFrame( int levelTime ) {
                     if ( level.warmupTime > 0 )
                             trap_SetConfigstring( CS_WARMUP, va("%i", level.warmupTime ) );
             }
+
+            // siege timer adjustment
+            char siegeState[128];
+            int round, startTime;
+            trap_GetConfigstring( CS_SIEGE_STATE, siegeState, sizeof( siegeState ) );
+            sscanf( siegeState, "%i|%i", &round, &startTime );
+            startTime += dt;
+            trap_SetConfigstring( CS_SIEGE_STATE, va( "%i|%i", round, startTime ) );
     }
     if ( level.pause.state == PAUSE_PAUSED )
     {
