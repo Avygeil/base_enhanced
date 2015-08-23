bbf5dfc9b5dd929d70989eab3709298648dac2f7
TheSil <sildak@gmail.com>
2015-08-23 10:13:49 +0200

    Reverted one of the changes made for #17, apparently it causes issues that didnt show up during testing

--- game/g_items.c
+++ game/g_items.c
@@ -239,8 +239,7 @@ void ShieldTouch(gentity_t *self, gentity_t *other, trace_t *trace)
 		// compare the parent's team to the "other's" team
 		if (self->parent && ( self->parent->client) && (other->client))
 		{
-			trap_Trace( trace, self->r.currentOrigin, self->r.mins, self->r.maxs, self->r.currentOrigin, self->s.number, other->clipmask );
-			if ( OnSameTeam( self->parent, other ) || (trace->startsolid) )
+			if ( OnSameTeam( self->parent, other ) )
 			{
 				ShieldGoNotSolid(self);
 			}
