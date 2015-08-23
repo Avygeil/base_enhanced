03d5ab2a1603365cb48037a5679112275886e08e
TheSil <sildak@gmail.com>
2015-08-23 18:06:44 +0200

    Another take on #17, but this time everything is different

--- game/g_items.c
+++ game/g_items.c
@@ -239,7 +239,8 @@ void ShieldTouch(gentity_t *self, gentity_t *other, trace_t *trace)
 		// compare the parent's team to the "other's" team
 		if (self->parent && ( self->parent->client) && (other->client))
 		{
-			if ( OnSameTeam( self->parent, other ) )
+			trap_Trace( trace, self->r.currentOrigin, self->r.mins, self->r.maxs, self->r.currentOrigin, self->s.number, other->clipmask );
+			if ( OnSameTeam( self->parent, other ) || (trace->startsolid && trace->entityNum == other->s.number) )
 			{
 				ShieldGoNotSolid(self);
 			}
