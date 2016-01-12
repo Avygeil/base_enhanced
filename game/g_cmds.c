f3cc5a6b185060a7d666e8ad130c2dda010410b1
Avygeil <avyg31l@gmail.com>
2016-01-12 15:45:33 +0100

    Fix message for non-openjk users

--- game/g_cmds.c
+++ game/g_cmds.c
@@ -843,7 +843,7 @@ void SetTeam( gentity_t *ent, char *s ) {
 	// Only check one way, so you can join spec back if you were forced as a passwordless spectator
 	if (team != TEAM_SPECTATOR && !client->pers.canJoin) {
 		trap_SendServerCommand( ent->client->ps.clientNum,
-			va("print \"^1You cannot join the game because your password is empty or incorrect.\n^8If you know the password, just use /password\n^8If you don't, stick around until someone gives it to you!\n\""));
+			va("print \"^1You cannot join the game because your password is empty or incorrect.\n^1If you know the password, just use /password\n^1If you don't, stick around until someone gives it to you!\n\""));
 
 		return;
 	}
