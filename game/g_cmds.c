907f40a7a36678d19d147738302bc1428ea96f7c
deathsythe47 <deathsythe47@gmail.com>
2016-04-12 19:17:59 -0500

    add 'M' chat token for current weapon's ammuninition

--- game/g_cmds.c
+++ game/g_cmds.c
@@ -1811,6 +1811,7 @@ static char* GetToken( gclient_t *cl, const char token ) {
 	case 'H': return va( "%d", Com_Clampi( 0, 999, cl->ps.stats[STAT_HEALTH] ) );
 	case 'A': return va( "%d", Com_Clampi( 0, 999, cl->ps.stats[STAT_ARMOR] ) );
 	case 'F': return va( "%d", Com_Clampi( 0, 999, cl->ps.fd.forcePower ) );
+	case 'M': return va( "%d", Com_Clampi( 0, 999, !weaponData[cl->ps.weapon].energyPerShot && !weaponData[cl->ps.weapon].energyPerShot ? 0 : cl->ps.ammo[weaponData[cl->ps.weapon].ammoIndex]));
 	case 'L': return GetLocation( cl );
 	default: return NULL;
 	}
