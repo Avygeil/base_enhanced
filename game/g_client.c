6ffd2f7c58a98680b928aebd0b2bd07752975207
Avygeil <avyg31l@gmail.com>
2016-08-13 15:47:19 +0200

    strip sensitive userinfo from logs

--- game/g_client.c
+++ game/g_client.c
@@ -1956,6 +1956,26 @@ qboolean PasswordMatches( const char *s ) {
 		( sv_privatepassword.string[0] && !strcmp( sv_privatepassword.string, s ) );
 }
 
+#define StripUserinfo( u, k )				\
+do {										\
+	if ( Info_ValueForKey( u, k ) ) {		\
+		Info_SetValueForKey( u, k, "***" );	\
+	}										\
+} while ( 0 )
+
+// strips sensitive info from userinfo and returns a static buffer for quick logging
+static char* GetStrippedUserinfo( char *userinfo ) {
+	static char cleanedUserinfo[MAX_INFO_STRING];
+
+	memcpy( cleanedUserinfo, userinfo, sizeof( cleanedUserinfo ) );
+	StripUserinfo( cleanedUserinfo, "ja_guid" );
+	StripUserinfo( cleanedUserinfo, "cuid" );
+	StripUserinfo( cleanedUserinfo, "sex" );
+	StripUserinfo( cleanedUserinfo, "password" );
+
+	return cleanedUserinfo;
+}
+
 /*
 ===========
 ClientUserInfoChanged
@@ -2356,14 +2376,14 @@ void ClientUserinfoChanged( int clientNum ) {
 			SHA1Input( &ctx, (unsigned char *)value, (unsigned int)strlen(value) );
 			if ( SHA1Result( &ctx ) == 1 ) {
 				guidHash = ctx.Message_Digest[0];
-				G_LogPrintf( "Client %d (OpenJK) reports guid %d (userinfo %s)\n", clientNum, guidHash, userinfo );
+				G_LogPrintf( "Client %d (OpenJK) reports guid %d (userinfo %s)\n", clientNum, guidHash, GetStrippedUserinfo( userinfo ) );
 			}
 		} else {
 			// not an openjk client, so we should be able to force their cvars
 			value = Info_ValueForKey( userinfo, "sex" );
 			if ( value && *value && Q_isanumber( value ) ) {
 				guidHash = atoi( value );
-				G_LogPrintf( "Client %d reports guid %d (userinfo %s)\n", clientNum, guidHash, userinfo );
+				G_LogPrintf( "Client %d reports guid %d (userinfo %s)\n", clientNum, guidHash, GetStrippedUserinfo( userinfo ) );
 			} else {
 				char systeminfo[16384] = { 0 };
 				char previnfo[16384] = { 0 };
@@ -2387,10 +2407,10 @@ void ClientUserinfoChanged( int clientNum ) {
 					}
 				}
 				if ( prevGuid ) {
-					G_LogPrintf( "Reassigning previously assigned guid %d to client %d (userinfo %s)\n", guidHash, clientNum, userinfo );
+					G_LogPrintf( "Reassigning previously assigned guid %d to client %d (userinfo %s)\n", guidHash, clientNum, GetStrippedUserinfo( userinfo ) );
 				} else {
 					guidHash = rand();
-					G_LogPrintf( "Assigning random guid %d to client %d (userinfo %s)\n", guidHash, clientNum, userinfo );
+					G_LogPrintf( "Assigning random guid %d to client %d (userinfo %s)\n", guidHash, clientNum, GetStrippedUserinfo( userinfo ) );
 				}
 				trap_GetConfigstring( CS_SYSTEMINFO, systeminfo, sizeof( systeminfo ) );
 
