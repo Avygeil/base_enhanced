499bd89c3a55524b980bb451d861db8ccd3bad9c
deathsythe47 <deathsythe47@gmail.com>
2016-11-16 17:05:22 -0800

    send id of guy getting votekicked

--- game/g_cmds.c
+++ game/g_cmds.c
@@ -2676,7 +2676,9 @@ const char *G_GetArenaInfoByMap( const char *map );
 int G_GetArenaNumber( const char *map );
 
 static int      g_votedCounts[MAX_ARENAS];
-
+#ifdef NEWMOD_SUPPORT
+#define KICKVOTE_FAKEOWNER	1022
+#endif
 void Cmd_CallVote_f( gentity_t *ent ) {
 	int		i;
 	static char	arg1[MAX_STRING_TOKENS];
@@ -2963,6 +2965,11 @@ void Cmd_CallVote_f( gentity_t *ent ) {
 			return;
 		}
 			
+#ifdef NEWMOD_SUPPORT
+		gentity_t *te = G_TempEntity(vec3_origin, EV_SIEGESPEC);
+		te->s.owner = KICKVOTE_FAKEOWNER;
+		te->s.eventParm = n;
+#endif
 		Com_sprintf ( level.voteString, sizeof(level.voteString ), "%s %i", arg1, n );
 		Com_sprintf ( level.voteDisplayString, sizeof(level.voteDisplayString), "kick %s", g_entities[n].client->pers.netname );
 	}
@@ -2986,7 +2993,11 @@ void Cmd_CallVote_f( gentity_t *ent ) {
 				return;
 			}
 		}
-
+#ifdef NEWMOD_SUPPORT
+		gentity_t *te = G_TempEntity(vec3_origin, EV_SIEGESPEC);
+		te->s.owner = KICKVOTE_FAKEOWNER;
+		te->s.eventParm = clientid;
+#endif
 		Com_sprintf ( level.voteString, sizeof(level.voteString ), "clientkick %d", clientid );
 		Com_sprintf ( level.voteDisplayString, sizeof(level.voteDisplayString), "kick %s", g_entities[clientid].client->pers.netname );
 	}
