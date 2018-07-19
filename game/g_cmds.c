f637e86d56f039d7a6ec743fb1e4610f01a2a961
Avygeil <avyg31l@gmail.com>
2018-07-19 17:12:05 +0200

    voice_cmd without argument prints the available commands, useful for binding

--- game/g_cmds.c
+++ game/g_cmds.c
@@ -2110,19 +2110,25 @@ static void Cmd_VoiceCommand_f(gentity_t *ent)
 		// list available arguments to help users bind stuff (otherwise the only way is to use assets/code)
 		
 		char voiceCmdList[MAX_STRING_CHARS];
+		Q_strncpyz( voiceCmdList, S_COLOR_WHITE"Available voice_cmd commands:", sizeof( voiceCmdList ) );
 
 		while ( i < MAX_CUSTOM_SIEGE_SOUNDS ) {
 			if ( !bg_customSiegeSoundNames[i] ) {
 				break;
 			}
 
-			if ( !Q_stricmp( bg_customSiegeSoundNames[i], s ) ) {
-				break;
+			if ( i % 8 == 0 ) { // line break every 8 commands
+				Q_strcat( voiceCmdList, sizeof( voiceCmdList ), "\n    "S_COLOR_YELLOW );
 			}
 
-			i++;
+			Q_strcat( voiceCmdList, sizeof( voiceCmdList ), bg_customSiegeSoundNames[i] + 1 ); // skip the * character
+			Q_strcat( voiceCmdList, sizeof( voiceCmdList ), " " );
+
+			++i;
 		}
 
+
+		trap_SendServerCommand( ent - g_entities, va( "print \"%s\n\"", voiceCmdList ) );
 		return;
 	}
 
