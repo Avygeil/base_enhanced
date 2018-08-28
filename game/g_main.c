66bd0206667efb06f15abc83f7b72ffe85da0cce
Avygeil <avyg31l@gmail.com>
2018-08-28 17:51:42 +0200

    more optimal default flag protection radius

--- game/g_main.c
+++ game/g_main.c
@@ -636,7 +636,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_maxNameLength, "g_maxNameLength", "24", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_duplicateNamesId, "g_duplicateNamesId", "1", CVAR_ARCHIVE, 0, qtrue },
 
-	{ &g_droppedFlagSpawnProtectionRadius, "g_droppedFlagSpawnProtectionRadius", "768", CVAR_ARCHIVE, 0, qtrue },
+	{ &g_droppedFlagSpawnProtectionRadius, "g_droppedFlagSpawnProtectionRadius", "1024", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_droppedFlagSpawnProtectionDuration, "g_droppedFlagSpawnProtectionDuration", "10000", CVAR_ARCHIVE, 0, qtrue },
 
 #ifdef NEWMOD_SUPPORT
