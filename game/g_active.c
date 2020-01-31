a77291daf481932ec07452981b4680541782eb79
deathsythe47 <deathsythe47@gmail.com>
2020-01-31 10:13:04 -0800

    don't send post-lag messages too rapidly

--- game/g_active.c
+++ game/g_active.c
@@ -4031,10 +4031,16 @@ void ClientEndFrame( gentity_t *ent ) {
 			ent->client->ps.eFlags |= EF_CONNECTION;
 	}
 	else {
-		if (ent->client->ps.eFlags & EF_CONNECTION || ent->client->isLagging) {
-			G_BroadcastServerFeatureList(ent - g_entities); // he was lagging (or vid_restarted) but isn't anymore; send this again just to be sure
-			if ( ent->client->sess.isInkognito )
-				trap_SendServerCommand( ent - g_entities, "kls -1 -1 \"inko\" \"1\"" ); // only update if enabled
+		if (ent->client->ps.eFlags & EF_CONNECTION || ent->client->isLagging) { // he was lagging (or vid_restarted) but isn't anymore; send this again just to be sure
+			// try not to send these post-lag messages too rapidly
+			static int lastTimePostLagMessagesSent[MAX_CLIENTS] = { 0 };
+			int now = trap_Milliseconds();
+			if (!lastTimePostLagMessagesSent[ent - g_entities] || now - lastTimePostLagMessagesSent[ent - g_entities] >= 3000) {
+				G_BroadcastServerFeatureList(ent - g_entities);
+				if (ent->client->sess.isInkognito)
+					trap_SendServerCommand(ent - g_entities, "kls -1 -1 \"inko\" \"1\""); // only update if enabled
+				lastTimePostLagMessagesSent[ent - g_entities] = now;
+			}
 		}
 		ent->client->isLagging = qfalse;
 		ent->client->ps.eFlags &= ~EF_CONNECTION;
