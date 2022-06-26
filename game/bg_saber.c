a86138c3c90c705f6f884d40291e564b8c2a7179
deathsythe47 <deathsythe47@gmail.com>
2022-06-26 11:50:51 -0700

    derp

--- game/bg_saber.c
+++ game/bg_saber.c
@@ -2084,7 +2084,7 @@ qboolean ShittySaberMoveEnabled(int move) {
 	if (pm->ps->clientNum >= MAX_CLIENTS)
 		return qtrue;
 
-	if (g_allowMoveDisable.integer & (1 << move))
+	if (!(g_allowMoveDisable.integer & (1 << move)))
 		return qtrue;
 
 	if (level.clients[pm->ps->clientNum].sess.disableShittySaberMoves & (1 << move))
