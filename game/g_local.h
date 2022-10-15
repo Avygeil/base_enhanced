4037c624f94483ec6629758d9a05eb39db333a31
deathsythe47@gmail.com <deathsythe47@gmail.com>
2022-10-15 07:51:59 -0400

    cherry picking is hard

--- game/g_local.h
+++ game/g_local.h
@@ -3050,6 +3050,8 @@ extern vmCvar_t     g_hackLog;
 
 extern vmCvar_t     g_fixPitKills;
 
+extern vmCvar_t     g_fixDempSaberThrow;
+
 extern vmCvar_t		g_fixForceJumpAnimationLock;
 extern vmCvar_t		g_fixNoAmmoShootAnimation;
 extern vmCvar_t		g_fix5AmmoSniping;
