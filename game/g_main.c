e048ba65305837c9db95dcd40a48c4404885c000
deathsythe47@gmail.com <deathsythe47@gmail.com>
2024-01-24 08:04:29 -0500

    default g_unlaggedFix to 0

--- game/g_main.c
+++ game/g_main.c
@@ -1259,7 +1259,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_unlaggedFactor, "g_unlaggedFactor", "0.25", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_unlaggedOffset, "g_unlaggedOffset", "0", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_unlaggedDebug, "g_unlaggedDebug", "0", CVAR_ARCHIVE, 0, qtrue },
-	{ &g_unlaggedFix, "g_unlaggedFix", "1", CVAR_ARCHIVE, 0, qtrue },
+	{ &g_unlaggedFix, "g_unlaggedFix", "0", CVAR_ARCHIVE, 0, qtrue },
 #endif
 
 	{ &g_printCountry, "g_printCountry", "1", CVAR_ARCHIVE, 0, qtrue },
